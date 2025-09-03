073241d9562c95f297292945599e5459d71faf7b
JohnLennonForXbox <230184197+JohnLennonForXbox@users.noreply.github.com>
2025-09-03 19:28:28 +0000

    First version

---
+++ script/index.js
@@ -0,0 +1,60 @@
+import JohnGrades from './johngrades.js';
+
+let JohnsClicked = 0;
+let EquippedLennon = "John Lennon";
+let JohnsPerClick = 1;
+
+const button = document.getElementById('JohnLennon');
+console.log(button);
+const heading = document.getElementById('JohnsClicked');
+button.addEventListener('click', () => {
+    ClickJohn();
+});
+button.addEventListener("contextmenu", function(event) {
+    event.preventDefault(); // Prevents the default right-click context menu
+});
+
+function ClickJohn() {
+    JohnsClicked += JohnsPerClick;
+    heading.textContent = `Johns clicked: ${JohnsClicked}`;
+    PlayJohnSound();
+}
+
+function PlayJohnSound() {
+    const JohnSound =  new Audio('/audio/John lennon.wav');
+    JohnSound.playbackRate = Math.random() * (2.0 - 0.5) + 0.5;
+    console.log(JohnSound.playbackRate);
+    JohnSound.play();
+}
+
+const JohngradeContainer = document.getElementById('JohngradeContainer');
+for (const name in JohnGrades) {
+    const Johngrade = document.createElement('div');
+    Johngrade.className = 'Johngrade';
+    Johngrade.innerHTML = `
+        <button class="${JohnGrades[name].Owned ? "owned" : ''} ${JohnGrades[name].Equipped ? "equipped" : '' }" id="${name}">
+            <h3>${name}</h3>
+            <img src="${JohnGrades[name].Image}" alt="${name}" class="Johngrade-image">
+            <p>${JohnGrades[name].Description}</p>
+            <p>Price: ${JohnGrades[name].Price} Johns</p>
+            <p>Johns per click: ${JohnGrades[name].JohnClicks}</p>
+        </button>
+    `
+    JohngradeContainer.appendChild(Johngrade);
+    Johngrade.addEventListener('click', () => {
+        if (!JohnGrades[name].Owned && JohnsClicked >= JohnGrades[name].Price) {
+            JohnsClicked -= JohnGrades[name].Price;
+            heading.textContent = `Johns clicked: ${JohnsClicked}`;
+            JohnGrades[name].Owned = true;
+            document.getElementById(name).classList.add('owned');
+        } else if (JohnGrades[name].Owned && !JohnGrades[name].Equipped) {
+            JohnGrades[EquippedLennon].Equipped = false;
+            document.getElementById(EquippedLennon).classList.remove('equipped');
+            EquippedLennon = name;
+            JohnsPerClick = JohnGrades[name].JohnClicks;
+            document.getElementById('JohnLennonImage').src = JohnGrades[name].Image;
+            JohnGrades[name].Equipped = true;
+            document.getElementById(name).classList.add('equipped');
+        }
+    });
+}
---
+++ script/johngrades.js
@@ -0,0 +1,76 @@
+var JohnGrades = {
+    "John Lennon": {
+        Price: 0,
+        JohnClicks: 1,
+        Owned: true,
+        Equipped: true,
+        Image: "./img/jlennon.png",
+        Description: "The original John. Click him to get more Johns!",
+    },
+    "Stone Lennon": {
+        Price: 10,
+        JohnClicks: 2,
+        Owned: false,
+        Equipped: false,
+        Image: "./img/stone lennon.png",
+        Description: "Stone statue of John Lennon, he breaks apart and gives you 2 Johns per click",
+    },
+    "Iron Lennon": {
+        Price: 100,
+        JohnClicks: 5,
+        Owned: false,
+        Equipped: false,
+        Image: "./img/iron lennon.png",
+        Description: "Cast iron model of John Lennon, he has a little john encapsulated in his head that gives you 5 Johns per click",
+    },
+    "Gold Lennon": {
+        Price: 1000,
+        JohnClicks: 10,
+        Owned: false,
+        Equipped: false,
+        Image: "./img/gold lennon.png",
+        Description: "So shiny he blinds you and the game director and he gives you 10 Johns per click by mistake because hes so shiny",
+    },
+    "Diamond Lennon": {
+        Price: 10000,
+        JohnClicks: 50,
+        Owned: false,
+        Equipped: false,
+        Image: "./img/diamond lennon.png",
+        Description: "A diamond so hard it cuts through your screen and gives you 50 Johns per click",
+    },
+    "Emerald Lennon": {
+        Price: 100000,
+        JohnClicks: 100,
+        Owned: false,
+        Equipped: false,
+        Image: "./img/emerald lennon.png",
+        Description: "Got it from some guy that looks like squidward, his ear is a crank that dispenses 100 Johns per click",
+    },
+    "Ruby Lennon": {
+        Price: 1000000,
+        JohnClicks: 500,
+        Owned: false,
+        Equipped: false,
+        Image: "./img/ruby lennon.png",
+        Description: "How many hue shifts can i get away with? This one gives you 500 Johns per click",
+    },
+    "Amethyst Lennon": {
+        Price: 10000000,
+        JohnClicks: 1000,
+        Owned: false,
+        Equipped: false,
+        Image: "./img/amethyst lennon.png",
+        Description: "I'm running out of things to wrte for these 1000 Johns per click",
+    },
+    "Invincible Lennon": {
+        Price: 100000000,
+        JohnClicks: 5000,
+        Owned: false,
+        Equipped: false,
+        Image: "./img/invincible lennon.png",
+        Description: "He's in- TITLECARD and gives you 5000 Johns per click",
+    }
+}
+
+export default JohnGrades;
